56592bf38877558e618114c03ea6644414632efb
Raj <raj1998211@gmail.com>
2019-07-20 01:26:09 +0530

    virtual env added

--- SR/settings.py
+++ SR/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 # Managing media - by raj
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
